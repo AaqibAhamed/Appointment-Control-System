c0c87a47ad677fbf069eadc1cdcc3601525428e1
Aaqib <aaqibgahamed@gmail.com>
2021-01-24 09:48:53 +0530

    Update Migrations

---
+++ AppointmentControlSystem/Migrations/20210124041654_Finalize.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppointmentControlSystem.Migrations
+{
+    public partial class Finalize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Appointment");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Appointment",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
